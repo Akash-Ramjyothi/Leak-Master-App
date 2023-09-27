d19b33f2a0d14755e86141357a70ac1a1e621260
Akash-Ramjyothi <akash.ramjyothi@gmail.com>
2023-09-27 12:08:30 +0530

    Added Toast Message

--- app/src/main/java/com/akash/leakmasterv2/MainActivity.kt
+++ app/src/main/java/com/akash/leakmasterv2/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.akash.leakmasterv2
 
 import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Debug
+import android.telephony.SmsManager
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import android.Manifest
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -17,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var dbRef: DatabaseReference // Creating reference for Database
     val TAG = "DEBUG TAG" // TAG for Log debugging
     private lateinit var gasSensorValuesList: ArrayList<GasSensorValuesModel> // Array list to store fetched data
+    private val PERMISSION_REQUEST_CODE = 123 // You can use any code you like
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference() // Creating reference for Firebase
@@ -71,9 +78,23 @@ class MainActivity : AppCompatActivity() {
                         Log.d(TAG,"Finding MQ-2 Value: ${i.value}")
 
                         val gasSensorValue = i.getValue(GasSensorValuesModel::class.java) // Variable to get actual mq2Value data
-                        Log.d(TAG,"Correct MQ-2 Value: ${gasSensorValue?.mq2Value}") // Final correct result
+                        val mq2SensorValueAny = gasSensorValue?.mq2Value
+                        val mq2SensorValueInt = mq2SensorValueAny as? Number // Safe cast Any to Number
+                        Log.d(TAG,"Correct MQ-2 Value: ${mq2SensorValueInt}") // Final correct result
 
-                        mq2valueTextView.setText("MQ-2 Sensor value: ${gasSensorValue?.mq2Value}")
+                        mq2valueTextView.setText("MQ-2 Sensor value: ${mq2SensorValueInt}") // Displaying MQ-2 sensor value in TextView
+
+                        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.SEND_SMS), PERMISSION_REQUEST_CODE)
+                        }
+
+                        // Send Alert SMS when MQ-2 Sensor value is above threshold eg: >= 5
+                        if(mq2SensorValueInt!=null && mq2SensorValueInt.toInt() >= 5){
+                            Toast.makeText(applicationContext, "ALERT!!! Gas Leak Datected", Toast.LENGTH_SHORT).show() // Toast message
+
+                            var smsObject = SmsManager.getDefault() // Create object for SMS Manager
+                            smsObject.sendTextMessage("8939928002",null,"ALERT!!! Gas Leak Datected",null,null) // Function to send SMS
+                        }
                     }
 
 //                    for (leakValues in  snapshot.children){
